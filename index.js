9d952a4bf6b3dab0c313c7d6cb4029d603ceb570
Garrett Halstein <garrett.halstein@emory.edu>
2019-01-19 05:54:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value){
   return newObj;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key] = value
+  object[key] = value;
+  return object;
 }
