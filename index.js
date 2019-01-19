7752cbda79ba701e89374f6b99808e09df9e3449
Garrett Halstein <garrett.halstein@emory.edu>
2019-01-19 05:55:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = Object();
 function updateObjectWithKeyAndValue(object, key, value){
-  var newObj = Object.assign({}, object, key, value);
+  var newObj = Object.assign({}, object);
+  newObj[key] = value;
   return newObj;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
